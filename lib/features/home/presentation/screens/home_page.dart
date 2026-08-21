1b2b151cfdb515eed239e31f7a29f7a72d079ba4
3mad Hany <emadhany975@gmail.com>
2026-08-21 18:06:23 +0300

    Add Windows runner support with DPI awareness and console output
    - Introduced a manifest file for the Windows runner to enable PerMonitorV2 DPI awareness.
    - Implemented console creation and output redirection in utils.cpp to support debugging.
    - Added utility functions for command line argument retrieval and UTF-16 to UTF-8 string conversion.
    - Developed Win32Window class to manage high DPI-aware windows, including theme updates based on system settings.
    - Created header files for utility functions and Win32Window class to encapsulate functionality.

---
+++ lib/features/home/presentation/screens/home_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Instagram'),
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
+        ],
+      ),
+
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(8),
+                      width: 80,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 2,
+                      right: 2,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                          shape: BoxShape.circle,
+                          color: Colors.grey[300],
+                        ),
+                        child: Icon(Icons.add, color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+
+                Container(
+                  padding: EdgeInsets.all(4),
+                  margin: EdgeInsets.all(8),
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.purple,
+                        Colors.purple,
+                        Colors.purple,
+                        Colors.red,
+                        Colors.red,
+                        Colors.orange,
+                        Colors.yellow,
+                        Colors.yellow,
+                      ],
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                    ),
+                  ),
+                  child: Container(
+                    height: 75,
+                    width: 75,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 3),
+                      shape: BoxShape.circle,
+                      color: Colors.grey,
+                    ),
+                    child: Icon(Icons.person, size: 40),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
